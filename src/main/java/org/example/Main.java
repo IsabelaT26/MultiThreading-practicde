7ea5006f6a7c4024b6483d097726abac7bec49a3
IsabelaT26 <isat.vasconcelos.sweden@gmail.com>
2026-09-09 15:25:56 +0200

    4.0 now i can pause thread 2 and resume it without interrupting or waiting a specific time

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,34 +8,36 @@ public class Main {
         T1 t1 = new T1();
         t1.start();
 
-        try {
-            Thread.sleep(5000);
-        }catch (InterruptedException e){
-            System.out.println("Thread interrupted");
-        }
+        wait5Seconds();
 
         T2 task = new T2();
         Thread t2 = new Thread(task);
         t2.start();
 
 
-        try {
-            Thread.sleep(5000);
-        }catch (InterruptedException e){
-            System.out.println("Thread interrupted");
-        }
+        wait5Seconds();
+
+        task.pause();
+
+        wait5Seconds();
+
+        task.resume();
+
+        wait5Seconds();
 
         t1.interrupt();
 
+        wait5Seconds();
+
+        t2.interrupt();
+
+    }
+
+    public static void wait5Seconds(){
         try {
             Thread.sleep(5000);
         }catch (InterruptedException e){
             System.out.println("Thread interrupted");
         }
-
-        t2.interrupt();
-
-
-
     }
 }
--- src/main/java/org/example/T2.java
+++ src/main/java/org/example/T2.java
@@ -2,9 +2,22 @@ package org.example;
 
 public class T2 implements Runnable{
 
+    private boolean paused = false;
+
+
     @Override
     public void run() {
         while (true) {
+
+            synchronized (this) {
+                while (paused) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                }
+            }
 
             System.out.println("Tråd 2");
 
@@ -13,6 +26,19 @@ public class T2 implements Runnable{
             } catch (InterruptedException e) {
                 break;
             }
+        }
+    }
+
+    public void pause(){
+        synchronized (this) {
+            paused = true;
+        }
+    }
+
+    public void resume(){
+        synchronized (this) {
+            paused = false;
+            notify();
         }
     }
 
